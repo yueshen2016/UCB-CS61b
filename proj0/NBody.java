19c592b285bf16ff81d73ae35bf9963b5ebf26bc
Yue Shen <yueshen2016@g.ucla.edu>
2021-10-19 20:38:44 -0700

    change function name from readBodies to readPlanets

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -6,7 +6,7 @@ public class NBody{
 		return radius;
 	}
 
-	public static Planet[] readBodies(String filename){
+	public static Planet[] readPlanets(String filename){
 		In in = new In(filename);
 		int numberOfPlanets = in.readInt();
 		double radius = in.readDouble();
@@ -30,7 +30,7 @@ public class NBody{
 		double dt = Double.parseDouble(args[1]);
 		String filename = args[2];
 		double radius = readRadius(filename);
-		Planet[] bodies = readBodies(filename);
+		Planet[] bodies = readPlanets(filename);
 
 		StdDraw.enableDoubleBuffering();
 		StdDraw.setScale((-1) * radius, radius);
